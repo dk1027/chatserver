227e73d1950c7e7efc4fa542c59f18db6d1f9203
Long Tse <ltse@urthecast.com>
2018-09-28 14:23:39 -0700

    first version direct msg

---
+++ src/chat.py
@@ -0,0 +1,88 @@
+import asyncio
+import json
+import websockets
+
+ACTIVE_USERS = dict()
+
+# TODO: What happens when my chat partner doens't exist? went offline?
+# TODO: input validation...
+
+
+async def register(websocket):
+    print('new connection')
+
+
+async def unregister(this_user):
+    try:
+        ACTIVE_USERS.pop(this_user)
+        print('connection closed')
+        print(f"active users {ACTIVE_USERS.keys()}")
+    except Exception as e:
+        print(f"{this_user} doesn't exist in ACTIVE_USERS??")
+
+
+def user_init(payload, websocket):
+    """
+    payload: {'user': 'your-user-name'}
+    """
+    try:
+        ACTIVE_USERS[payload['user']] = websocket
+    except Exception as e:
+        print(f"malformed payload? {payload}")
+
+
+async def broadcast_msg(msg):
+    websockets = [ACTIVE_USERS[k] for k in ACTIVE_USERS.keys()]
+    await asyncio.wait([ws.send(msg) for ws in websockets])
+
+
+async def direct_msg(payload):
+    """
+    payload:
+    {
+        'target_user' :'other person username'
+        'msg': 'str' # TODO: Impose msg limit?
+    }
+    """
+    try:
+        target_user = payload['target_user']
+        msg = payload['msg']
+        ws = ACTIVE_USERS[target_user]
+        await asyncio.wait([ws.send(msg)])
+    except Exception as e:
+        print(str(e))
+        print("payload", payload)
+        print("ACTIVE_USERS", ACTIVE_USERS.keys())
+
+
+async def hello(websocket, path):
+    await register(websocket)
+    try:
+        need_init = True
+        this_user = 'unknown'
+        async for message in websocket:
+            if need_init:
+                # first message is always the initialize payload
+                # e.g. {'user': 'your-user-name'}
+                data = json.loads(message)
+                print(data)
+                user_init(data, websocket)
+                this_user = data['user']
+                print(f'this user: {this_user}')
+                msg = f"new connection: active users {ACTIVE_USERS.keys()}"
+                print(msg)
+                await broadcast_msg(msg)
+                need_init = False
+            # from here on, assume its all chat messages
+            print(message)
+            await direct_msg(json.loads(message))
+    finally:
+        # REVISIT: why is there a stack trace when the client disconnects?
+        # However server isn't exitting..... maybe there's a connection handler
+        # I need to define?
+        await unregister(this_user)
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ src/tests/test_something.py
@@ -0,0 +1,6 @@
+# import pytest
+
+
+class TestSomething:
+    def test_something_method(self):
+        assert 'hello' == 'hello'
